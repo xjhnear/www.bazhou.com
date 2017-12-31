a1ff70d19759145ec0c0da8117633dc4f43fe051
Near <250327864@qq.com>
2017-12-31 19:03:49 +0800

    f
    Signed-off-by: Near <250327864@qq.com>

--- apps/api/controllers/UserController.php
+++ apps/api/controllers/UserController.php
@@ -233,12 +233,12 @@ class UserController extends BaseController
 		if($urid <= 0 || !$numbers){
 			return $this->fail(202,'参数异常');
 		}
-
-        $fullPath = public_path().'/downloads/info/';
-
-        $myfile = fopen($fullPath.'newfile.txt', "w+") or die("Unable to open file!");
-        fwrite($myfile, json_encode($_FILES));
-        fclose($myfile);
+//
+//        $fullPath = public_path().'/downloads/info/';
+//
+//        $myfile = fopen($fullPath.'newfile.txt', "w+") or die("Unable to open file!");
+//        fwrite($myfile, json_encode($_FILES));
+//        fclose($myfile);
 
         if(Input::hasFile('video')){
             $video = MyHelp::save_img_no_url(Input::file('video'),'video');
